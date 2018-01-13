eda7f952d693761d55d6e79e7875afcb57a39c48
nicolasapb <nicolas.a.p.barbosa@gmail.com>
2018-01-12 23:38:21 -0200

    aula 3 - video 2

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,12 @@
 class NegociacaoController {
     adiciona(event) {
         event.preventDefault()
-        alert('kek')
+        const $ = document.querySelector.bind(document)
+        let inputData = $('#data')
+        let inputQuantidade = $('#quantidade')
+        let inputValor = $('#valor')
+        console.log('data', inputData.value)
+        console.log('quantidade', inputQuantidade.value)
+        console.log('valor', inputValor.value)
     }
 }
