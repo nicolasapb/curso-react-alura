a3044ec85daa9322241147d8e1b437e2f636a539
nicolasapb <nicolas.a.p.barbosa@gmail.com>
2018-01-17 11:51:25 -0200

    modulo 2 - aula 1 - video 3

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,7 +5,10 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
 
-        this._listaNegociacoes = new ListaNegociacoes()
+        this._listaNegociacoes = new ListaNegociacoes(this, function(model) {
+            this._negociacoesView.update(model)
+        })
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'))
         this._negociacoesView.update(this._listaNegociacoes)
 
@@ -18,12 +21,18 @@ class NegociacaoController {
         event.preventDefault()
 
         this._listaNegociacoes.adiciona(this._criaNegociacao())
-        this._negociacoesView.update(this._listaNegociacoes)
 
         this._mensagem.texto = 'Negociação adicionada com sucesso'
         this._mensagemView.update(this._mensagem)
 
         this._limpaFormulario()
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia()
+
+        this._mensagem.texto = 'Negociaçõe apagadas com sucesso'
+        this._mensagemView.update(this._mensagem)
     }
 
     _criaNegociacao() {
--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,22 @@
 class ListaNegociacoes {
-    constructor() {
+    constructor(contexto, armadilha) {
         this._negociacoes = []
+        this._armadilha = armadilha
+        this._contexto = contexto
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao)
+            // this._armadilha(this)
+        Reflect.apply(this._armadilha, this._contexto, [this])
     }
 
     get negociacoes() {
         return [].concat(this._negociacoes)
+    }
+
+    esvazia() {
+        this._negociacoes = []
+        Reflect.apply(this._armadilha, this._contexto, [this])
     }
 }
