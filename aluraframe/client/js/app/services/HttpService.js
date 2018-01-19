d99423a4411f83223d10dc4bc896e939979d9ef5
nicolasapb <nicolas.a.p.barbosa@gmail.com>
2018-01-19 21:44:26 -0200

    modulo 3 - aula 5 - video 2

--- aluraframe/client/js/app/services/HttpService.js
+++ aluraframe/client/js/app/services/HttpService.js
@@ -1,46 +1,23 @@
 class HttpService {
+
+    _handleError(res) {
+        if (!res.ok)
+            throw new Error(res.statusText)
+        return res
+    }
+
     get(url) {
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest()
-
-            xhr.open('GET', url, true)
-
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        resolve(JSON.parse(xhr.responseText))
-                    } else {
-                        reject(xhr.responseText)
-                    }
-                }
-            }
-
-            xhr.send()
-        })
+        return fetch(url)
+            .then(res => this._handleError(res))
+            .then(res => res.json())
     }
 
     post(url, dado) {
-
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado));
-        });
-
+        return fetch(url, {
+                headers: { 'Content-type': 'application/json' },
+                method: 'POST',
+                body: JSON.stringify(dado)
+            })
+            .then(res => this._handleError(res))
     }
 }
