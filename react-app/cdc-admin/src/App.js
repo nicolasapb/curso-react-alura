180bc22ec35b635d6677e930ba177dde133c8959
nicolasapb <nicolas.a.p.barbosa@gmail.com>
2018-02-01 23:18:19 -0200

    react 1.4.4

--- react-app/cdc-admin/src/App.js
+++ react-app/cdc-admin/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from "jquery";
+import InputCustomizado from "./componentes/InputCustomizado";
 
 class App extends Component {
 
@@ -81,23 +82,14 @@ class App extends Component {
                     <div className="content" id="content">
                     <div className="pure-form pure-form-aligned">
                       <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                        <InputCustomizado label="Nome"  id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
+                        <InputCustomizado label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
+                        <InputCustomizado label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} /> 
                         <div className="pure-control-group">
-                                    <label htmlFor="nome">Nome</label>
-                                    <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>
-                                </div>
-                                <div className="pure-control-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
-                                </div>
-                                <div className="pure-control-group">
-                                    <label htmlFor="senha">Senha</label>
-                                    <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}/>
-                                </div>
-                                <div className="pure-control-group">
-                                    <label></label>
-                                    <button type="submit" className="pure-button pure-button-primary">Gravar
-                                    </button>
-                                </div>
+                            <label></label>
+                            <button type="submit" className="pure-button pure-button-primary">Gravar
+                            </button>
+                        </div>
                       </form>           
 
                     </div>  
