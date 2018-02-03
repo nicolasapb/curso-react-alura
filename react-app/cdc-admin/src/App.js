b835fd86c5a528bfaaeff4a0bc03536db85a7048
nicolasapb <nicolas.a.p.barbosa@gmail.com>
2018-02-03 13:31:06 -0200

    react 1.5.2

--- react-app/cdc-admin/src/App.js
+++ react-app/cdc-admin/src/App.js
@@ -1,61 +1,9 @@
 import React, { Component } from 'react'; 
 import './css/pure-min.css';
-import './css/side-menu.css';
-import $ from "jquery";
-import InputCustomizado from "./componentes/InputCustomizado";
-import SubmitCustom from "./componentes/SubmitCustom";
+import './css/side-menu.css'; 
+import AutorBox from "./Autor";
 
 class App extends Component {
-
-    constructor() {
-        super()
-        this.state = {lista: [], nome: '', email: '', senha: ''}
-        this.enviaForm = this.enviaForm.bind(this)
-        this.setNome = this.setNome.bind(this);
-        this.setEmail = this.setEmail.bind(this);
-        this.setSenha = this.setSenha.bind(this);
-    }
-
-    componentDidMount() {
-        $.ajax({
-            url: "http://localhost:8080/api/autores",
-            dataType: "json",
-            success: (resposta) => {  
-                this.setState({lista: resposta})
-            }
-        })
-    }
-
-    enviaForm(evento) {
-        evento.preventDefault() 
-        $.ajax({
-            url: "http://localhost:8080/api/autores",
-            contentType: "application/json",
-            dataType: "json",
-            type: 'post',
-            data: JSON.stringify({
-                nome: this.state.nome, 
-                email: this.state.email, 
-                senha: this.state.senha
-            }),
-            success: resposta =>  {
-                this.setState({lista: resposta})
-            },
-            erro: resposta => console.log("erro", resposta) 
-        })
-    }
-
-    setNome(evento){
-        this.setState({nome:evento.target.value});
-    }
-
-    setEmail(evento){
-        this.setState({email:evento.target.value});
-    }
-
-    setSenha(evento){
-        this.setState({senha:evento.target.value});
-    }
 
     render() {
         return ( 
@@ -63,11 +11,9 @@ class App extends Component {
                 <a href="#menu" id="menuLink" className="menu-link">
                     <span></span>
                 </a>
-
                 <div id="menu">
                     <div className="pure-menu">
                         <a className="pure-menu-heading" href="#company">Company</a>
-
                         <ul className="pure-menu-list"> 
                             <li className="pure-menu-item"><a href="#home" className="pure-menu-link">Home</a></li>
                             <li className="pure-menu-item"><a href="#autor" className="pure-menu-link">Autor</a></li>
@@ -78,34 +24,10 @@ class App extends Component {
 
                 <div id="main">
                     <div className="header">
-                    <h1>Cadastro de Autores</h1>
+                        <h1>Cadastro de Autores</h1>
                     </div>
-                    <div className="content" id="content">
-                    <div className="pure-form pure-form-aligned">
-                      <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                        <InputCustomizado label="Nome"  id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
-                        <InputCustomizado label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
-                        <InputCustomizado label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} /> 
-                        <SubmitCustom label="Gravar" type="submit"/>
-                      </form>           
-
-                    </div>  
-                    <div>            
-                        <table className="pure-table">
-                        <thead>
-                            <tr>
-                            <th>Nome</th>
-                            <th>email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.lista.map(autor => <tr key={autor.id}><td>{autor.nome}</td><td>{autor.email}</td></tr> )
-                            }
-                        </tbody>
-                        </table> 
-                    </div>             
-                    </div>
+                    <br/>
+                    <AutorBox/>
                 </div> 
             </div>
         )
---
+++ react-app/cdc-admin/src/Autor.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import InputCustomizado from "./componentes/InputCustomizado";
+import SubmitCustom from "./componentes/SubmitCustom";
+
+export default class AutorBox extends Component {
+
+    constructor() {
+        super()
+        this.state = { lista: [] }
+        this.atualizaLista = this.atualizaLista.bind(this)
+    }
+
+    componentDidMount() {
+        $.ajax({
+            url: "http://localhost:8080/api/autores",
+            dataType: "json",
+            success: resposta => {
+                this.setState({ lista: resposta })
+            }
+        })
+    }
+
+    atualizaLista(novaLista) {
+        this.setState({lista:novaLista})
+    }
+
+    render() {
+        return ( 
+            <div className="content" id="content">
+                <FormularioAutor callbackAtualizaLista={this.atualizaLista}/>
+                <TabelaAutores lista={this.state.lista}/>
+            </div> 
+        )
+    }
+}
+
+class FormularioAutor extends Component {
+
+    constructor() {
+        super()
+        this.state = { nome: '', email: '', senha: '' }
+        this.enviaForm = this.enviaForm.bind(this)
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }    
+
+    enviaForm(evento) {
+        evento.preventDefault()
+        $.ajax({
+            url: "http://localhost:8080/api/autores",
+            contentType: "application/json",
+            dataType: "json",
+            type: 'post',
+            data: JSON.stringify({
+                nome: this.state.nome,
+                email: this.state.email,
+                senha: this.state.senha
+            }),
+            success: resposta => {
+                this.props.callbackAtualizaLista(resposta)
+            },
+            erro: resposta => console.log("erro", resposta)
+        })
+    }
+
+    setNome(evento) {
+        this.setState({ nome: evento.target.value });
+    }
+
+    setEmail(evento) {
+        this.setState({ email: evento.target.value });
+    }
+
+    setSenha(evento) {
+        this.setState({ senha: evento.target.value });
+    }
+    
+    render() {
+        return (
+            <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                    <InputCustomizado label="Nome" id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
+                    <InputCustomizado label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
+                    <InputCustomizado label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
+                    <SubmitCustom label="Gravar" type="submit" />
+                </form>
+            </div>             
+        )
+    }
+}
+
+class TabelaAutores extends Component {
+
+    render() {
+        return (
+            <div>
+                <table className="pure-table">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.lista.map(autor => <tr key={autor.id}><td>{autor.nome}</td><td>{autor.email}</td></tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>               
+        )
+    }
+}
