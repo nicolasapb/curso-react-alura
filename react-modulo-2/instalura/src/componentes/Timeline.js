ec9165b66ab38cfa8938c3dfdb7efb35710b449d
nicolasapb <nicolas.a.p.barbosa@gmail.com>
2018-02-20 22:36:16 -0300

    react 2.2.1

--- react-modulo-2/instalura/src/componentes/Timeline.js
+++ react-modulo-2/instalura/src/componentes/Timeline.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import Foto from "./Foto";
 
 export default class Timeline extends Component {
+
+    componentDidMount() {
+        fetch('http://localhost:8080/api/public/fotos/alots')
+        .then(resp => resp.text())
+        .then(texto => JSON.parse(texto))
+        .then(resultado => resultado.map(a => console.log(a)))
+        .catch(erro => console.log(erro))
+    }
+
     render() {
         return (
             <div className="fotos container">
