b056e0296ece54ebc807890680686524860e06ef
nicolasapb <nicolas.a.p.barbosa@gmail.com>
2018-02-23 21:44:04 -0300

    react 2.3.1

--- react-modulo-2/instalura/src/index.js
+++ react-modulo-2/instalura/src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/reset.css'
 import './css/timeline.css'
+import './css/login.css'
 import App from './App';
+import Login from "./componentes/Login";
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (<Router>
+        <Switch>
+            <Route exact path="/" component={Login}/>
+            <Route exact path="/timeline" component={App}/>
+        </Switch>
+    </Router>), 
+
+    document.getElementById('root')
+);
+
 registerServiceWorker();
